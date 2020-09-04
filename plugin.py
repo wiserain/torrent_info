f380fa3254382b4d26f50833a1d8892eda95ac93
wiserain <mail275@gmail.com>
2020-09-04 10:26:46 +0900

    0.0.8.4
    - libtorrent 1.2.10

--- plugin.py
+++ plugin.py
@@ -40,7 +40,7 @@ def plugin_unload():
 
 plugin_info = {
     "category_name": "torrent",
-    "version": "0.0.8.3",
+    "version": "0.0.8.4",
     "name": "torrent_info",
     "home": "https://github.com/wiserain/torrent_info",
     "more": "https://github.com/wiserain/torrent_info",
@@ -48,7 +48,7 @@ plugin_info = {
     "developer": "wiserain",
     "zip": "https://github.com/wiserain/torrent_info/archive/master.zip",
     "icon": "",
-    "install": "1.2.9-200821",
+    "install": "1.2.10-200903",
 }
 #########################################################
 
